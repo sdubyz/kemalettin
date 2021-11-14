a2a79e983b46d1ba830b4f8bdd2dc2c25fc7d0ce
sdubyz1 <45362195+sdubyz@users.noreply.github.com>
2021-11-14 11:50:10 +0000

    Indentation mistakes fixed.

--- main.py
+++ main.py
@@ -161,27 +161,27 @@ async def pause(ctx):
     if (lab_file == "wrong"):
         return
     else:
-        with open(lab_file) as f:
-            for line in f:
-				pass
-		if(line.startswith("Mola")):
-			await ctx.send("Zaten Moladasınız!")
-			return
-		else:
-			last_line = line
-			now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-			log_file = open(lab_file, "a")
-			log_file.write("Mola başlangıç " + now + "\n")
-			log_file.close()
-			FMT = '%H:%M:%S'
-			tot_time = str(datetime.strptime(
-				now[-8:], FMT) - datetime.strptime(last_line[-9:-1], FMT))
-			tot_fi_name = check_user(lab_file)
-			tot_file = open(tot_fi_name, "a")
-			tot_file.write(tot_time + "\n")
-			tot_file.close()
-			await ctx.send("Süre duraklatıldı!")
-			await ctx.send("Çalıştığınız Süre: " + tot_time)
+      with open(lab_file) as f:
+          for line in f:
+            pass
+      if (line.startswith("Mola")):
+        await ctx.send("Zaten Moladasınız!")
+        return
+      else:
+        last_line = line
+        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        log_file = open(lab_file, "a")
+        log_file.write("Mola başlangıç " + now + "\n")
+        log_file.close()
+        FMT = '%H:%M:%S'
+        tot_time = str(datetime.strptime(
+          now[-8:], FMT) - datetime.strptime(last_line[-9:-1], FMT))
+        tot_fi_name = check_user(lab_file)
+        tot_file = open(tot_fi_name, "a")
+        tot_file.write(tot_time + "\n")
+        tot_file.close()
+        await ctx.send("Süre duraklatıldı!")
+        await ctx.send("Çalıştığınız Süre: " + tot_time)
 
 client.add_command(pause)
 
