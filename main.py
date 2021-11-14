5e80f1371e68c87856073316bb9e827d041042f0
Harun Gumus <harungumusm@gmail.com>
2021-11-14 16:17:18 +0300

    addition of working times

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ async def stop(ctx):
         tot_time = datetime.strptime("00:00:00", FMT)
         with open(tot_file) as f:
             for line in f:
-                print(str(datetime.strptime(line[:-1], FMT)))
+                tot_time += datetime.strptime(line[:-1], FMT)
 
         diff = str(datetime.strptime(
             now[-8:], FMT) - datetime.strptime(start[-9:-1], FMT))
@@ -246,7 +246,7 @@ async def stop(ctx):
             start[-9:-1], FMT) + timedelta(hours=3)
         now_t = datetime.strptime(
             now[-8:], FMT) + timedelta(hours=3)
-        await ctx.send("Toplam Çalışma süresi: " + diff)
+        await ctx.send("Toplam Çalışma süresi: " + tot_time)
         await ctx.send("Başlangıç: " + str_time.strftime("%H:%M:%S") + ", Bitiş: " + now_t.strftime("%H:%M:%S"))
 
 client.add_command(stop)
