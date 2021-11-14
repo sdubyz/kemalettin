60a0be5ab2411c396425c15c8ee0aff66e832063
Harun Gumus <harungumusm@gmail.com>
2021-11-14 14:10:19 +0300

    check_valid functiona added to main.py

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ from discord import Color
 from datetime import datetime
 from check_user import check_valid
 
-music = discord.Activity(type=discord.ActivityType.listening, name ="Her Halini Severim")
-client = commands.Bot(command_prefix="!", status=discord.Status.online, activity=music)
+music = discord.Activity(
+    type=discord.ActivityType.listening, name="Her Halini Severim")
+client = commands.Bot(command_prefix="!",
+                      status=discord.Status.online, activity=music)
+
 
 @client.event
 async def on_ready():
@@ -29,78 +32,83 @@ red = Color.red()
 prev = [n]
 file1.close()
 
+
 @tasks.loop(seconds=20)
 async def rates():
     channel = client.get_channel(901924767513329695)
     channel2 = client.get_channel(902109009329389598)
     #ch_labne = client.get_channel(838136630425944066)
 
-    dollar = float(str(currency()).replace(",","."))
+    dollar = float(str(currency()).replace(",", "."))
     up = '<:greenUp:902101687043514378> '
     down = '<:redDown:902101653845594142> '
 
     up2 = '<:greenUp:902110266278424606> '
     down2 = '<:redDown:902110295986679860> '
 
-    diff = abs(((dollar - prev[0]) / prev[0] )*100)
+    diff = abs(((dollar - prev[0]) / prev[0])*100)
     if(diff <= 0.025):
-      pass
+        pass
     else:
-      if (prev[0] <= dollar):
-        pos_ratio = '```diff\nDolar: {} ₺\n+ %{}\n```'.format(dollar, round(diff,4))
-        em = discord.Embed(color=green, title ="")
-        em.add_field(name=up*6, value=pos_ratio)
+        if (prev[0] <= dollar):
+            pos_ratio = '```diff\nDolar: {} ₺\n+ %{}\n```'.format(
+                dollar, round(diff, 4))
+            em = discord.Embed(color=green, title="")
+            em.add_field(name=up*6, value=pos_ratio)
 
-        em2 = discord.Embed(color=green, title="")
-        em2.add_field(name=up2*6 ,value=pos_ratio)
-      else:
-        neg_ratio = '```diff\nDolar: {} ₺\n- %{}\n```'.format(dollar, round(diff,4))
-        em = discord.Embed(color=red, title="")
-        em.add_field(name=down*6, value=neg_ratio)
+            em2 = discord.Embed(color=green, title="")
+            em2.add_field(name=up2*6, value=pos_ratio)
+        else:
+            neg_ratio = '```diff\nDolar: {} ₺\n- %{}\n```'.format(
+                dollar, round(diff, 4))
+            em = discord.Embed(color=red, title="")
+            em.add_field(name=down*6, value=neg_ratio)
 
-        em2 = discord.Embed(color=red, title="")
-        em2.add_field(name=down2*6, value=neg_ratio)
+            em2 = discord.Embed(color=red, title="")
+            em2.add_field(name=down2*6, value=neg_ratio)
 
-      prev.pop(0)
-      prev.append(dollar)
-      file1 = open(nameFile, "w")
-      file1.write(str(dollar))
-      file1.close()
-      em3 = discord.Embed(color=red, title="")
-      em3.add_field(name="DOLAR 10!!!", value="2023'e hazırız")
-      rte_gif = "https://media.giphy.com/media/ELFInaH0V34jNkBdTb/giphy.gif"
-      em3.set_image(url=rte_gif)
-      
-      #if(dollar >= 10):
-      #  await ch_labne.send(embed=em3)
+        prev.pop(0)
+        prev.append(dollar)
+        file1 = open(nameFile, "w")
+        file1.write(str(dollar))
+        file1.close()
+        em3 = discord.Embed(color=red, title="")
+        em3.add_field(name="DOLAR 10!!!", value="2023'e hazırız")
+        rte_gif = "https://media.giphy.com/media/ELFInaH0V34jNkBdTb/giphy.gif"
+        em3.set_image(url=rte_gif)
 
-      await channel2.send(embed=em2)
-      await channel.send(embed=em)
-      print("Sent!")
+        # if(dollar >= 10):
+        #  await ch_labne.send(embed=em3)
+
+        await channel2.send(embed=em2)
+        await channel.send(embed=em)
+        print("Sent!")
 
 
 @tasks.loop(minutes=5)
 async def daily_update():
-  channel = client.get_channel(901924767513329695)
-  difference = daily()
-  await channel.edit(topic = "Günlük aralık: " + difference)
+    channel = client.get_channel(901924767513329695)
+    difference = daily()
+    await channel.edit(topic="Günlük aralık: " + difference)
 
 
 @tasks.loop(minutes=6)
 async def ch_name():
     channel = client.get_channel(901924767513329695)
-    dollar_num = round(float(currency().replace(',','.')), 2)
-    dollar = str(dollar_num).replace('.','-')
+    dollar_num = round(float(currency().replace(',', '.')), 2)
+    dollar = str(dollar_num).replace('.', '-')
     await channel.edit(name="dolar-{}".format(dollar))
+
 
 @commands.command()
 async def curr(ctx):
     dollar = currency()
     send_this = "💲: {} ₺".format(dollar)
-    em = discord.Embed(color = green, title = send_this)
+    em = discord.Embed(color=green, title=send_this)
     await ctx.send(embed=em)
 
 client.add_command(curr)
+
 
 @commands.command()
 async def say(ctx, *args):
@@ -124,117 +132,106 @@ async def leave(ctx):
 client.add_command(leave)
 
 
-lab_lib_ch = [909170792347107358, 901055167825346600, 845613490000494602, 845613443851485184, 845613337236602942, 525024033381810179]
+lab_lib_ch = [909170792347107358, 901055167825346600, 845613490000494602,
+              845613443851485184, 845613337236602942, 525024033381810179]
 ir_id = 522835029596831774
 guz_id = 522825818225901578
 hra_id = 462700306724290563
 
+
 @commands.command()
 async def start(ctx):
-  lab_file = check_valid(ctx)
-  log_file = open(lab_file, "w")
-  log_file.write("Başlangıç " + datetime.now().strftime("%m-%d-%Y %H:%M:%S") +"\n")
-  log_file.close()
-  await ctx.send("İyi çalışmalar!")
+    lab_file = check_valid(ctx)
+    if (lab_file == "wrong"):
+        return
+    else:
+        log_file = open(lab_file, "w")
+        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        log_file.write("Başlangıç " + now + "\n")
+        log_file.close()
+        await ctx.send("İyi çalışmalar!")
 
 
 client.add_command(start)
 
+
 @commands.command()
 async def pause(ctx):
-  if ctx.message.channel.id in lab_lib_ch:
-    if ctx.message.author.id == ir_id:
-      lab_file = "irem.txt"
-    elif ctx.message.author.id == guz_id:
-      lab_file = "guzi.txt"
-    elif ctx.message.author.id == hra_id:
-      lab_file = "hra.txt"
+    lab_file = check_valid(ctx)
+    if (lab_file == "wrong"):
+        return
     else:
-      return
-    with open(lab_file) as f:
-      for line in f:
-        if (line.startswith("Başlangıç")):
-          last_line = line
-        else:
-          continue
-    #print(last_line[-9:])
-    f.close()
-    now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-    log_file = open(lab_file, "a")
-    log_file.write("Mola başlangıç " + now +"\n")
-    log_file.close()
-    FMT = '%H:%M:%S'
-    #print(datetime.strptime(last_line[-9:-1], FMT).strftime("%H:%M:%S"))
-    await ctx.send("Süre duraklatıldı!")
-    await ctx.send("Çalıştığınız Süre: "+str(datetime.strptime(now[-8:], FMT) - datetime.strptime(last_line[-9:-1], FMT)))
-  else:
-    return
+        with open(lab_file) as f:
+            for line in f:
+                if (line.startswith("Başlangıç")):
+                    last_line = line
+                else:
+                    continue
+        f.close()
+        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        log_file = open(lab_file, "a")
+        log_file.write("Mola başlangıç " + now + "\n")
+        log_file.close()
+        FMT = '%H:%M:%S'
+        #print(datetime.strptime(last_line[-9:-1], FMT).strftime("%H:%M:%S"))
+        await ctx.send("Süre duraklatıldı!")
+        await ctx.send("Çalıştığınız Süre: "+str(datetime.strptime(now[-8:], FMT) - datetime.strptime(last_line[-9:-1], FMT)))
 
 client.add_command(pause)
 
+
 @commands.command()
 async def cont(ctx):
-  if ctx.message.channel.id in lab_lib_ch:
-    if ctx.message.author.id == ir_id:
-      lab_file = "irem.txt"
-    elif ctx.message.author.id == guz_id:
-      lab_file = "guzi.txt"
-    elif ctx.message.author.id == hra_id:
-      lab_file = "hra.txt"
+    lab_file = check_valid(ctx)
+    if(lab_file == "wrong"):
+        return
     else:
-      return
-    with open(lab_file) as f:
-      for line in f:
-        if (line.startswith("Mola")):
-          last_line = line
-        else:
-          continue
-    #print(last_line[-9:])
-    f.close()    
-    FMT = '%H:%M:%S'
-    now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")    
-    log_file = open(lab_file, "a")
-    log_file.write("Başlangıç " + now +"\n")
-    log_file.close()
-    await ctx.send("İyi çalışmalar!")
-    diff = str(datetime.strptime(now[-8:], FMT) - datetime.strptime(last_line[-9:-1], FMT))
-    await ctx.send("Mola Süresi: "+ diff)
-  else:
-    return
+        with open(lab_file) as f:
+            for line in f:
+                if (line.startswith("Mola")):
+                    last_line = line
+                else:
+                    continue
+        # print(last_line[-9:])
+        f.close()
+        FMT = '%H:%M:%S'
+        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        log_file = open(lab_file, "a")
+        log_file.write("Başlangıç " + now + "\n")
+        log_file.close()
+        await ctx.send("İyi çalışmalar!")
+        diff = str(datetime.strptime(
+            now[-8:], FMT) - datetime.strptime(last_line[-9:-1], FMT))
+        await ctx.send("Mola Süresi: " + diff)
 
 client.add_command(cont)
 
+
 @commands.command()
 async def stop(ctx):
-  if ctx.message.channel.id in lab_lib_ch:
-    if ctx.message.author.id == ir_id:
-      lab_file = "irem.txt"
-    elif ctx.message.author.id == guz_id:
-      lab_file = "guzi.txt"
-    elif ctx.message.author.id == hra_id:
-      lab_file = "hra.txt"
+    lab_file = check_valid(ctx)
+    if(lab_file == "wrong"):
+        return
     else:
-      return
-    with open(lab_file) as f:
-      for line in f:
-        pass
-      last_line = line
-    f.close()   
+        with open(lab_file) as f:
+            for line in f:
+                pass
+            last_line = line
+        f.close()
 
-    f = open(lab_file, "r")
-    start = f.readline()
-    f.close()      
-    FMT = '%H:%M:%S'
-    now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")    
-    log_file = open(lab_file, "a")
-    log_file.write("Bitiş " + now +"\n")
-    log_file.close()
-    await ctx.send("")
-    diff = str(datetime.strptime(now[-8:], FMT) - datetime.strptime(start[-9:-1], FMT))
-    await ctx.send("Toplam Çalışma süresi: "+ diff)
-    await ctx.send("Başlangıç: " + last_line[-9:-1] +", Bitiş: "+ now[-8:])
-  else:
-    return
+        f = open(lab_file, "r")
+        start = f.readline()
+        f.close()
+        FMT = '%H:%M:%S'
+        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        log_file = open(lab_file, "a")
+        log_file.write("Bitiş " + now + "\n")
+        log_file.close()
+        await ctx.send("")
+        diff = str(datetime.strptime(
+            now[-8:], FMT) - datetime.strptime(start[-9:-1], FMT))
+        await ctx.send("Toplam Çalışma süresi: " + diff)
+        await ctx.send("Başlangıç: " + last_line[-9:-1] + ", Bitiş: " + now[-8:])
 
 client.add_command(stop)
 
