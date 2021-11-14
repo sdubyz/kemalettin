032af13d9f8fc0723d4079891f6e5666b2cf746a
Harun Gumus <harungumusm@gmail.com>
2021-11-14 15:58:24 +0300

    check time

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ async def stop(ctx):
         tot_time = datetime.strptime("00:00:00", FMT)
         with open(tot_file):
             for line in tot_file:
-                tot_time += datetime.strptime(line, FMT)
+                print(str(datetime.strptime(line, FMT)))
 
         diff = str(datetime.strptime(
             now[-8:], FMT) - datetime.strptime(start[-9:-1], FMT))
